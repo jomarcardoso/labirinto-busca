131faca020d2f1913c60730f272b5040bac6ac92
Jomar <jomar.cardsoso@cwi.com.br>
2020-09-27 10:20:35 -0300

    a asterisco

--- src/BuscaAAsterisco.java
+++ src/BuscaAAsterisco.java
@@ -1,12 +1,25 @@
 import Labirinto.*;
+import java.util.Iterator;
+import java.util.Vector;
 
 public class BuscaAAsterisco extends Busca {
   void prepararBusca() {
-
+    Posicao posicaoInicial = this.labirinto.getPosicaoEntrada();
+    this.caminho.add(posicaoInicial);
   }
 
   void buscar() {
-    System.out.println("percorridos: " + this.percorridos);
+    Posicao ultimaPosicao = this.caminho.lastElement();
+    Vector<Posicao> expansao = labirinto.getExpansao(ultimaPosicao);
+
+    Iterator<Posicao> expansaoIterator = expansao.iterator();
+
+    while (expansaoIterator.hasNext()) {
+      Posicao expansaoItem = (Posicao) expansaoIterator.next();
+      System.out.println("expansao: " + labirinto.getDLR(expansaoItem, labirinto.getPosicaoSaida()));
+    }
+
+    this.achouOFinal = true;
   }
 
 	public static void main(String[] args) {
